5f2cba0ca4dbf98e16a4fa2449ce810fb32a6211
EC2 Default User <117354218+ayumu3221@users.noreply.github.com>
2022-12-09 08:52:35 +0000

    Finish

--- app/controllers/searchs_controller.rb
+++ app/controllers/searchs_controller.rb
@@ -1,25 +1,13 @@
 class SearchsController < ApplicationController
+  before_action :authenticate_user!
   def search
-    @model = params["model"]
-    @content = params["content"]
-    @method = params["method"]
-    @records = search_for(@model, @content, @method)
-  end
-
-  private
-  def search_for(model, content, method)
-    if model == 'user'
-      if method == 'perfect'
-        User.where(name: content)
-      else
-        User.where('name LIKE ?', '%'+content+'%')
-      end
-    elsif model == 'book'
-      if method == 'perfect'
-        Post.where(title: content)
-      else
-        Post.where('title LIKE ?', '%'+content+'%')
-      end
+    @model = params[:model]
+    @content = params[:content]
+    @method = params[:method]
+    if @model == 'user'
+    @records = User.search_for(@content, @method)
+    else
+    @records = Book.search_for(@content, @method)
     end
   end
 end
